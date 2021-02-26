98b230c339bf6b3ffbe0bae8bb96f6f6bdec0bb7
ppusher <thelastjediwarrior-cs50@yahoo.com>
2021-02-26 14:29:14 +0530

    removed unused code

--- responsive_web_developer/edie-homepage/script.js
+++ responsive_web_developer/edie-homepage/script.js
@@ -3,20 +3,6 @@ const navOpen = document.querySelector('.ham-open');
 const navClose = document.querySelector('.ham-close');
 const hamNav = document.querySelector('.ham-nav')
 
-// hamNav.addEventListener('click', openNav )
-
-// function openNav() {
-//   if (navOpen.style.display = "block") {
-//     navbar.style.visibility = "visible";
-//     navClose.style.display = "block";
-//     navOpen.style.display = "none";
-//   } else if (navClose.style.display="block"){
-//     navbar.style.visibility = "hidden";
-//     navClose.style.display = "none";
-//     navOpen.style.display = "block";
-//   }
-// }
-
 navOpen.addEventListener('click', openNav)
 navClose.addEventListener('click', closeNav)
 
