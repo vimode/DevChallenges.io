74668f0a3eb7e90a2cc60a4a4bb84725f1dc3af8
ppusher <thelastjediwarrior-cs50@yahoo.com>
2021-03-05 18:23:50 +0530

    Added mobile nav

--- responsive_web_developer/portfolio-page/script.js
+++ responsive_web_developer/portfolio-page/script.js
@@ -1,1 +1,24 @@
 const button = document.querySelector('.btn');
+const navOpen = document.querySelector('#open-nav');
+const navClose = document.querySelector('#close-nav');
+const navItems = document.querySelector('.nav-items');
+
+navOpen.addEventListener('click', openNav)
+navClose.addEventListener('click', closeNav)
+
+function openNav() {
+  navItems.style.visibility = "visible";
+  navClose.style.display = "block";
+  navOpen.style.display = "none";
+}
+
+function closeNav() {
+  navItems.style.visibility = "hidden";
+  navClose.style.display = "none"; 
+  navOpen.style.display = "block";
+}
+
+const navLink = document.querySelectorAll('.nav-item');
+for (link of navLink) {
+  link.addEventListener('click', closeNav)
+}
