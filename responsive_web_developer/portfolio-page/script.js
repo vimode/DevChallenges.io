c02936f8d70b999adc93104addf3c3c4bff974b4
ppusher <thelastjediwarrior-cs50@yahoo.com>
2021-03-06 16:42:11 +0530

    Portfolio Page : Added tag filter js

--- responsive_web_developer/portfolio-page/script.js
+++ responsive_web_developer/portfolio-page/script.js
@@ -1,3 +1,4 @@
+// NAVBAR
 const button = document.querySelector('.btn');
 const navOpen = document.querySelector('#open-nav');
 const navClose = document.querySelector('#close-nav');
@@ -22,3 +23,56 @@ const navLink = document.querySelectorAll('.nav-item');
 for (link of navLink) {
   link.addEventListener('click', closeNav)
 }
+
+// PROJECT FILTER
+
+const filters  = document.querySelectorAll('.project-tab-item');
+
+const html = document.querySelector('.htmlproject');
+const css = document.querySelector('.cssproject');
+const responsive = document.querySelector('.responsiveproject');
+
+filters.forEach((filter) => {
+  filter.addEventListener('click', ()=> {
+    let filterValue = filter.innerText;
+    if(filterValue === "CSS") {
+      html.classList.add('hidden');
+      css.classList.remove('hidden');
+      responsive.classList.add('hidden')
+    } else if (filterValue === "HTML") {
+      html.classList.remove('hidden')
+      css.classList.add('hidden');
+      responsive.classList.add('hidden')
+    } else if (filterValue === "Responsive") {
+      html.classList.add('hidden')
+      css.classList.add('hidden');
+      responsive.classList.remove('hidden')
+    } else if (filterValue === "All") {
+      html.classList.remove('hidden')
+      css.classList.remove('hidden');
+      responsive.classList.remove('hidden')
+    }
+  })
+})
+
+
+// filters.forEach((filter)=> {
+//   filter.addEventListener('click', () => {
+//     let filterValue = filter.innerText;
+//     console.log(filterValue)
+
+//     if (filterValue == "CSS") {
+//         css.forEach((item)=> {
+//           css.classList.toggle('hidden')
+//     })
+//       } else if (filterValue == "Responsive") {
+//         responsive.forEach((item)=> {
+//           item.classList.toggle('hidden')
+//         })
+//       } else  {
+//         html.forEach((item)=> {
+//           item.classList.toggle('hidden');
+//         })
+//       }
+//   })
+// })
